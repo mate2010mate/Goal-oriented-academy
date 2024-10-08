a72e4730392e118c7aaddbb868e228267fb3ac41
mate2010mate <matenibladze2@gmail.com>
2024-10-08 19:56:49 +0400

    commit

---
+++ Level 43/homework/1.py
@@ -0,0 +1,9 @@
+def sum_of_list(num_list):
+    total = 0
+    for num in num_list:
+        total += num
+    return total
+
+# მაგალითი გამოყენება
+numbers = [1, 2, 3, 4, 5]
+print(sum_of_list(numbers))  # შედეგი: 15
