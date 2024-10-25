c61e6645d3b1c95331311f0bfc4150ccfda0c837
mate2010mate <matenibladze2@gmail.com>
2024-10-25 11:37:01 +0400

    commit

--- Level 45/homework/4.py
+++
@@ -1,9 +0,0 @@
-def calculate_average(numbers):
-    if len(numbers) == 0:
-        return "List is empty."
-    return sum(numbers) / len(numbers)
-
-# Example usage:
-numbers = [1, 2, 3, 4, 5, 6]
-average = calculate_average(numbers)
-print("Average:", average)
